d3970498076de26566a2fe8b0d28a8c5414414e5
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-04-20 12:40:09 -0500

    FEAT/added functionality for handling the run command (simply shows 'Running' as of now)

--- container.go
+++ container.go
@@ -5,6 +5,32 @@
 
 package main
 
-// This is the command we're trying to run:
-// docker run image <cmd> <params>
+import (
+	"os"
+	"fmt"
+)
 
+// This is the command we're trying to run (in docker) and this is the command we're going to parse:
+// docker              run image <cmd> <params>
+// go run container.go run       <cmd> <params>
+// the image is not going to get added 
+
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	
+	default:
+		panic("Unrecognized command, aborting.")
+	}
+}
+
+func run() {
+	fmt.Printf("Running")
+}
+
+func must(err error){
+	if err != nil {
+		panic(err)
+	}
+}
