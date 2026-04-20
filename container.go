e1a5b1e9a3c9cf38936c83de85937c95323e01b1
PyroSh0ck <parthmodi.amodi@gmail.com>
2026-04-20 11:35:28 -0500

    CHORE/setup go file

---
+++ container.go
@@ -0,0 +1,10 @@
+/*
+	Namespaces - controlling what the container can see and what they can't see. Basically restricting the container's
+	view of the host process. Created with syscalls
+*/
+
+package main
+
+// This is the command we're trying to run:
+// docker run image <cmd> <params>
+
